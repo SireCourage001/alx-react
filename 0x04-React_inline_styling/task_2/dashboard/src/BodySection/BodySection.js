9ef98605f2521eda66b829e24d992d052fa5470c
SireCourage001 <134224615+SireCourage001@users.noreply.github.com>
2024-06-04 20:29:24 +0100

    React_inline_styling

---
+++ 0x04-React_inline_styling/task_2/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+
+class BodySection extends React.Component{
+    render(){
+        const {children,title} = this.props
+        return(
+        <div className="bodySection">
+            <h2>{title}</h2>
+            {children}
+        </div>
+        )
+    }
+}
+
+
+export default BodySection
